48405e0b6b5cf8ed2eda524831a8420859d44897
nic <31355096+nabbi@users.noreply.github.com>
2023-12-24 16:39:22 -0600

    refresh Python3.11 (#1)
    * pylint 3.11
    * poetry isn't my thing
    * refactor tox and lint
    * travis to github actions
    * remove hound tox covers
    * py black tox
    * gh tox.ini
    * gh tox
    
    Signed-off-by: Nic Boet <nic@boet.cc>

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
+"""Setuptools for zm-py"""
 from setuptools import setup, find_packages
+import pathlib
 
+here = pathlib.Path(__file__).parent.resolve()
 
-long_description = open('README.md').read()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='zm-py',
-    version='0.5.2',
-    license='Apache Software License',
-    url='https://github.com/rohankapoorcom/zm-py',
-    author='Rohan Kapoor',
-    author_email='rohan@rohankapoor.com',
-    description='A loose python wrapper around the ZoneMinder REST API.',
+    name="zm-py",
+    version="0.5.2",
+    license="Apache Software License",
+    url="https://github.com/rohankapoorcom/zm-py",
+    author="Rohan Kapoor",
+    author_email="rohan@rohankapoor.com",
+    description="A loose python wrapper around the ZoneMinder REST API.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
-    install_requires=list(val.strip() for val in open('requirements.txt')),
+    platforms="any",
+    # install_requires=list(val.strip() for val in (here / 'requirements.txt').read_text(encoding="utf-8")),
+    install_requires=["requests"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
